68ca55a990271f4e472827f8d9f6d087ba464e35
Pietro Sette <settepietro33@gmail.com>
2022-03-04 17:58:02 -0800

    print remove

--- get_property_details.py
+++ get_property_details.py
@@ -344,7 +344,6 @@ def extract_images_from_listings(listings_dict):
 
 def map_external_room_label_to_internal(room):
     print("MAPPING")
-    print
     GENERAL_ROOMS = ['living_room', 'dining_room', 'bedroom']
     if room == 'kitchen':
         return 'kitchen'
